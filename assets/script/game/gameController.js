6e9c7f2d9737c5b11209e48d0e9d493e92536930
caoFly <caofei666@outlook.com>
2020-07-24 20:56:11 +0800

    游戏场景

--- assets/script/game/gameController.js
+++ assets/script/game/gameController.js
@@ -8,7 +8,12 @@ cc.Class({
             default: null
         },
 
-        NodeTouchBg: {
+        NodeBg: {
+            type: cc.Node,
+            default: null
+        },
+
+        NodeGrass: {
             type: cc.Node,
             default: null
         },
@@ -38,28 +43,37 @@ cc.Class({
     },
 
     update () {
+        this.moveBg();
+        this.moveGrass();
         this.moveCamera();
         this.controlPerson();
     },
 
     rigisterEvent () {
-        this.NodeTouchBg.on(cc.Node.EventType.TOUCH_START, this.onTouchScreenStart, this);
-        this.NodeTouchBg.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchScreenMove, this);
-        this.NodeTouchBg.on(cc.Node.EventType.TOUCH_END, this.onTouchScreenEnd, this);
-        this.NodeTouchBg.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchScreenCancel, this);
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchScreenStart, this);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchScreenMove, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchScreenEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchScreenCancel, this);
     },
 
     unregisterEvent () {
-        this.NodeTouchBg.off(cc.Node.EventType.TOUCH_START, this.onTouchScreenStart, this);
-        this.NodeTouchBg.off(cc.Node.EventType.TOUCH_MOVE, this.onTouchScreenMove, this);
-        this.NodeTouchBg.off(cc.Node.EventType.TOUCH_END, this.onTouchScreenEnd, this);
-        this.NodeTouchBg.off(cc.Node.EventType.TOUCH_END, this.onTouchScreenCancel, this);
+        this.node.off(cc.Node.EventType.TOUCH_START, this.onTouchScreenStart, this);
+        this.node.off(cc.Node.EventType.TOUCH_MOVE, this.onTouchScreenMove, this);
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchScreenEnd, this);
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchScreenCancel, this);
     },
 
     initPlayData () {
         this.moveDirX = 0;
         this.moveDirY = 0;
         this.baseVelocity = 500;
+        this.bgMoveStartX = -640;
+        this.bgMoveMinX = -3500;
+        this.bgMoveStep = 5;
+
+        this.grassMoveStartX = -640;
+        this.grassMoveMinX = -3500;
+        this.grassMoveStep = 10;
     },
 
     /***
@@ -67,16 +81,6 @@ cc.Class({
      */
     moveCamera () {
         if(!this.NodeCamera || !this.NodePerson) return;
-
-        if (this.NodePerson.x <= 0) {
-            this.NodeCamera.x = 0;
-        }
-        else if (this.NodePerson.x >= 3840) {
-            this.NodeCamera.x = 3840;
-        }
-        else {
-            this.NodeCamera.x = this.NodePerson.x;
-        }
 
         if (this.NodePerson.y <= 0) {
             this.NodeCamera.y = 0;
@@ -89,6 +93,29 @@ cc.Class({
         }
     },
 
+
+    /***
+     * 移动背景
+     */
+    moveBg () {
+        if(!this.NodeBg) return;
+        if(this.NodeBg.x <= this.bgMoveMinX) {
+            this.NodeBg.x = this.bgMoveStartX;
+        }
+        this.NodeBg.x -= this.bgMoveStep;
+    },
+
+    /***
+     * 移动草地
+     */
+    moveGrass () {
+        if(!this.NodeGrass) return;
+        if(this.NodeGrass.x <= this.grassMoveMinX) {
+            this.NodeGrass.x = this.grassMoveStartX;
+        }
+        this.NodeGrass.x -= this.grassMoveStep;
+    },
+
     controlPerson () {
         if(this.moveDirX === 0 && this.moveDirY === 0) return;
         this.NodePersonRigidBody.linearVelocity = cc.v2(this.moveDirX * this.baseVelocity, this.moveDirY * this.baseVelocity);
@@ -96,20 +123,19 @@ cc.Class({
 
 
     onTouchScreenStart (event) {
+        var self = this;
         var touches = event.getTouches();
-        var touchPoint = touches[0].getLocation();
-
-        var touchWorldPos = touchPoint.convertToWorldSpaceAR();
-
-        if (touchPoint.x > this.NodePerson.x) {
+        var startPos = touches[0].getStartLocation();
+        // var startPos = self.node.convertToNodeSpaceAR(touches[0].getStartLocation());
+        if (startPos.x > this.NodePerson.x) {
             this.moveDirX = 1;
         }
-        if (touchPoint.x < this.NodePerson.x) {
+        if (startPos.x < this.NodePerson.x) {
             this.moveDirX = -1;
         }
     },
 
-    onTouchScreenMove () {
+    onTouchScreenMove (event) {
 
     },
 
